c1a34d8dc15591850b7d1c8f76ab25861bf1c439
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-11-10 09:13:30 +0900

    router works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeComponent
+    HomeComponent,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
